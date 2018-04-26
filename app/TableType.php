56aba763d75fb03153544b32d058684f4219abd2
Makoto Chiba <hypermkt@gmail.com>
2018-04-27 02:39:29 +0900

    リレーションを設定

--- app/TableType.php
+++ app/TableType.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TableType extends Model
 {
-    //
+    public function courses()
+    {
+        return $this->belongsToMany('App\Course');
+    }
 }
