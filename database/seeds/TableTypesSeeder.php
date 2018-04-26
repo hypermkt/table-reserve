03eacb06d0572f9270143a7f7ad9d11e0e6439f6
Makoto Chiba <hypermkt@gmail.com>
2018-04-27 02:24:26 +0900

    シーディングを追加

---
+++ database/seeds/TableTypesSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TableTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('table_types')->insert([
+            'release_state' => 'public',
+            'title' => '小テーブル',
+            'start_time' => '17:00:00',
+            'end_time' => '23:00:00',
+            'minimum_capacity' => 1,
+            'max_capacity' => 2,
+            'number_of_sales' => 5,
+            'connectable' => true,
+        ]);
+
+        DB::table('table_types')->insert([
+            'release_state' => 'public',
+            'title' => '中テーブル',
+            'start_time' => '17:00:00',
+            'end_time' => '23:00:00',
+            'minimum_capacity' => 1,
+            'max_capacity' => 4,
+            'number_of_sales' => 5,
+            'connectable' => true,
+        ]);
+    }
+}
