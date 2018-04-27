7c42a22885603cb1af4d32dbd1f7b3d3b5a96957
Makoto Chiba <hypermkt@gmail.com>
2018-04-28 00:15:59 +0900

    席タイプ一覧を表示する

--- app/Http/Controllers/TableTypeController.php
+++ app/Http/Controllers/TableTypeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TableType;
 
 class TableTypeController extends Controller
 {
@@ -13,7 +14,7 @@ class TableTypeController extends Controller
      */
     public function index()
     {
-        //
+        return view('table_types.index', ['tableTypes' => TableType::all()]);
     }
 
     /**
