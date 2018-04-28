f4f39467035a852d48e64d615ccf289c235b6c54
Makoto Chiba <hypermkt@gmail.com>
2018-04-28 22:27:06 +0900

    削除機能を実装。但しforeign keyの都合でエラーが発生する問題有り

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -91,11 +91,13 @@ class CourseController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Course $course
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Course $course)
     {
-        //
+        $course->delete();
+
+        return redirect()->to('/courses');
     }
 }
