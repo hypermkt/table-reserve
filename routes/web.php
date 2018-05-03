57856f58fa150470792cf7881f92df53531c4db9
Makoto Chiba <hypermkt@gmail.com>
2018-05-03 12:15:23 +0900

    認証していなければ、各機能にアクセスできなくする

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('table_types', 'TableTypeController');
-Route::resource('courses', 'CourseController');
-Route::resource('stocks', 'StockController');
+Route::resource('table_types', 'TableTypeController')->middleware('auth');
+Route::resource('courses', 'CourseController')->middleware('auth');
+Route::resource('stocks', 'StockController')->middleware('auth');
 
 Route::get('auth/twitter', 'Auth\SocialLoginController@redirectToProvider');
 Route::get('auth/twitter/callback', 'Auth\SocialLoginController@handleProviderCallback');
