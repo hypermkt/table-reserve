fffdd62b83713ad44f671688a2687aed7607b354
Makoto Chiba <hypermkt@gmail.com>
2018-05-03 22:20:33 +0900

    予約コンポーネントの土台を追加

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('reservation-component', require('./components/ReservationComponent.vue'));
 
 const app = new Vue({
     el: '#app'
