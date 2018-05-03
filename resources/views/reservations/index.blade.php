94f83e5ae3a955b0d9ade35dcf2a2f261db1aeb5
Makoto Chiba <hypermkt@gmail.com>
2018-05-03 18:11:20 +0900

    メモを追加

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
     reservation.index
+    <p>TODO</p>
+    <ul>
+        <li>* 店舗情報の表示</li>
+        <li>* コース一覧の表示</li>
+    </ul>
 @endsection
--- resources/views/reservations/show.blade.php
+++ resources/views/reservations/show.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
     reservation.show
+    <ul>
+        <li>* コースの一覧の表示</li>
+        <li>* カレンダーの表示</li>
+        <li>* 時間の表示</li>
+        <li>* 氏名・メールアドレス・電話番号の入力</li>
+    </ul>
 @endsection
