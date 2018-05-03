21f8d9298caaf784309390ec07d95f168023d233
Makoto Chiba <hypermkt@gmail.com>
2018-05-03 22:04:25 +0900

    Vue.jsが動くように調整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,12 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <title>アプリ名 - @yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-<div class="container">
+<div id="app" class="container">
     @yield('content')
 </div>
+<script src="/js/app.js"></script>
 </body>
 </html>
