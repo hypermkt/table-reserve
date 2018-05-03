28a715bae34c381273b455257583883b4a43d4a0
Makoto Chiba <hypermkt@gmail.com>
2018-05-03 21:55:39 +0900

    メニュー一覧を追加

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -4,7 +4,26 @@
     reservation.index
     <p>TODO</p>
     <ul>
-        <li>* 店舗情報の表示</li>
-        <li>* コース一覧の表示</li>
+        <li>店舗情報の表示</li>
+        <li>コース一覧の表示</li>
     </ul>
+
+    <h2>コース</h2>
+    <table>
+        <tr>
+            <td>コース名</td>
+            <td>
+                <button>予約する</button>
+                <button>コース詳細</button>
+            </td>
+        </tr>
+        <tr>
+            <td>コース名</td>
+            <td>
+                <button>予約する</button>
+                <button>コース詳細</button>
+            </td>
+        </tr>
+    </table>
+
 @endsection
