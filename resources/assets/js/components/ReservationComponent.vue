f855b6a48c8766f80829bab1254e8991c48cdee0
Makoto Chiba <hypermkt@gmail.com>
2018-05-04 01:01:27 +0900

    不要な部分を削除

--- resources/assets/js/components/ReservationComponent.vue
+++ resources/assets/js/components/ReservationComponent.vue
@@ -17,9 +17,9 @@ export default {
     data() {
        return {
            selectedDate: [
-               new Date(2018, 4, 1), // Jan 1st, 2018
-               new Date(2018, 4, 15), // Jan 15th, 2018
-               new Date(2018, 4, 29) // Jan 29th, 2018
+               new Date(2018, 4, 1),
+               new Date(2018, 4, 15),
+               new Date(2018, 4, 29)
            ]
        }
     },
