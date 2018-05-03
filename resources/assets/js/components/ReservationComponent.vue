8920f982c865ae294e070f77c665885de385eb2d
Makoto Chiba <hypermkt@gmail.com>
2018-05-04 01:38:49 +0900

    ハイライト設定を追加

--- resources/assets/js/components/ReservationComponent.vue
+++ resources/assets/js/components/ReservationComponent.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <v-date-picker
-                mode='multiple'
+                mode='single'
                 v-model='selectedDate'
+                :attributes='attributes'
+                :disabled-dates='{ days: [1, 10, 20, 30] }'
                 is-inline>
         </v-date-picker>
         <p>selectedDate: {{ this.selectedDate }}</p>
@@ -16,10 +18,25 @@ import 'v-calendar/lib/v-calendar.min.css';
 export default {
     data() {
        return {
-           selectedDate: [
-               new Date(2018, 4, 1),
-               new Date(2018, 4, 15),
-               new Date(2018, 4, 29)
+           selectedDate: '',
+           attributes: [
+               {
+                   highlight: {
+                       backgroundColor: '#9f80ff',     // Purple background
+                       borderColor: '#8c66ff',
+                       borderWidth: '2px',
+                   },
+                   contentStyle: {
+                       color: 'white',                 // White text
+                   },
+                   dates: [
+                       new Date(2018, 4, 2),
+                       new Date(2018, 4, 3),
+                       new Date(2018, 4, 4),
+                       new Date(2018, 4, 5),
+                       new Date(2018, 4, 6),
+                   ],
+               },
            ]
        }
     },
