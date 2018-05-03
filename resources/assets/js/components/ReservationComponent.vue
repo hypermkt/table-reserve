b338231435855491bce87a7edee1dffb58625e79
Makoto Chiba <hypermkt@gmail.com>
2018-05-04 01:52:42 +0900

    Elementで雑に予約フォームを作成

--- resources/assets/js/components/ReservationComponent.vue
+++ resources/assets/js/components/ReservationComponent.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <ul>
+            <li><el-radio v-model="form.radio" label="1">Option A</el-radio></li>
+            <li><el-radio v-model="form.radio" label="2">Option B</el-radio></li>
+        </ul>
+
+        <br />
+        <br />
         <v-date-picker
                 mode='single'
                 v-model='selectedDate'
@@ -8,6 +15,31 @@
                 is-inline>
         </v-date-picker>
         <p>selectedDate: {{ this.selectedDate }}</p>
+
+        <el-button>10:00</el-button>
+        <el-button>10:30</el-button>
+        <el-button>11:00</el-button>
+        <el-button>11:30</el-button>
+        <el-button>12:00</el-button>
+
+        <el-form ref="form" :model="form" label-width="120px">
+            <el-form-item label="お名前">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+
+            <el-form-item label="メールアドレス">
+                <el-input v-model="form.email"></el-input>
+            </el-form-item>
+
+             <el-form-item label="電話番号">
+                <el-input v-model="form.tel"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">Create</el-button>
+                <el-button>Cancel</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 
@@ -19,6 +51,12 @@ export default {
     data() {
        return {
            selectedDate: '',
+           form: {
+               name: '',
+               email: '',
+               tel: '',
+               radio: '',
+           },
            attributes: [
                {
                    highlight: {
--- resources/views/reservations/show.blade.php
+++ resources/views/reservations/show.blade.php
@@ -1,14 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    reservation.show
-    <ul>
-        <li> コースの一覧の表示</li>
-        <li> カレンダーの表示</li>
-        <li> 時間の表示</li>
-        <li> 氏名・メールアドレス・電話番号の入力</li>
-    </ul>
-
     <h2>ネット予約</h2>
     <reservation-component></reservation-component>
 @endsection
