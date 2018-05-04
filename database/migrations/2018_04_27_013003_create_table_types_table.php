7369a1fd636476f1e324781e74346088aca63934
Makoto Chiba <hypermkt@gmail.com>
2018-05-04 21:34:51 +0900

    ページを削除したら関連する席タイプ、在庫設定も削除するようにする

--- database/migrations/2018_04_27_013003_create_table_types_table.php
+++ database/migrations/2018_04_27_013003_create_table_types_table.php
@@ -16,7 +16,7 @@ class CreateTableTypesTable extends Migration
         Schema::create('table_types', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('page_id')->unsigned();
-            $table->foreign('page_id')->references('id')->on('pages');
+            $table->foreign('page_id')->references('id')->on('pages')->onDelete('cascade');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->enum('release_state', ['public', 'private']); // 公開状態
--- database/migrations/2018_04_30_220943_create_stocks_table.php
+++ database/migrations/2018_04_30_220943_create_stocks_table.php
@@ -18,7 +18,7 @@ class CreateStocksTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->integer('table_type_id')->unsigned();
-            $table->foreign('table_type_id')->references('id')->on('table_types');
+            $table->foreign('table_type_id')->references('id')->on('table_types')->onDelete('cascade');;
             $table->date('accept_date');
             $table->integer('acceptable_table_number');
             $table->timestamps();
