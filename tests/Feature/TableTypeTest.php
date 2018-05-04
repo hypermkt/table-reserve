aa4c5f9aad48b37743e76edf0ebc6fa7754a847b
Makoto Chiba <hypermkt@gmail.com>
2018-05-04 20:29:35 +0900

    テストを修正

--- tests/Feature/TableTypeTest.php
+++ tests/Feature/TableTypeTest.php
@@ -8,30 +8,35 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TableTypeTest extends TestCase
 {
+    protected $page;
+
     public function setUp()
     {
         // MEMO: Laravelで独自のsetUpを呼ぶ場合は、parent::setUpの呼び出しが必要
         // refs: https://readouble.com/laravel/5.6/ja/testing.html
         parent::setUp();
         $user = factory(\App\User::class)->create();
+        $this->page = factory(\App\Page::class)->create([
+            'user_id' => $user->id
+        ]);
         $this->actingAs($user);
     }
 
     public function testIndex()
     {
-        $response = $this->get('/table_types');
+        $response = $this->get('/pages/' . $this->page->id . '/table_types');
         $response->assertStatus(200);
     }
 
     public function testCreate()
     {
-        $response = $this->get('/table_types/create');
+        $response = $this->get('/pages/' . $this->page->id . '/table_types/create');
         $response->assertStatus(200);
     }
 
     public function testStore()
     {
-        $response = $this->post('/table_types', [
+        $response = $this->post('/pages/' . $this->page->id . '/table_types', [
             'release_state' => 'public',
             'title' => 'hoge',
             'start_time' => '10:00',
@@ -41,6 +46,6 @@ class TableTypeTest extends TestCase
             'number_of_sales' => 3,
             'connectable' => 0,
         ]);
-        $response->assertRedirect('/table_types');
+        $response->assertRedirect('/pages/' . $this->page->id . '/table_types');
     }
 }
