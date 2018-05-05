5b21a26e469b25ea753f27678eda4740d9194a70
Makoto Chiba <hypermkt@gmail.com>
2018-05-05 18:19:49 +0900

    ページに紐づくコース一覧を返すエンドポイントを追加

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Course;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\CourseRequest;
 use App\Http\Controllers\Controller;
 
@@ -10,8 +10,15 @@ class CourseController extends Controller
 {
     public function index($pageId)
     {
-        return response()->json(
-            ['hello' => 'world']
-        );
+        $courses = DB::table('pages')
+            ->join('users', 'pages.user_id', '=', 'users.id')
+            ->join('courses', 'users.id', '=', 'courses.user_id')
+            ->where('pages.id', $pageId)
+            ->where('pages.release_state', 'public')
+            ->where('courses.release_state', 'public')
+            ->select('courses.*')
+            ->get();
+
+        return response()->json(['courses' => $courses]);
     }
 }
