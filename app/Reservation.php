18e6ddbea94ca323a5d950bb2480d58d197b5e4a
Makoto Chiba <hypermkt@gmail.com>
2018-05-05 13:19:36 +0900

    テーブル名を変更

--- app/Reservation.php
+++ app/Reservation.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Reserve extends Model
+class Reservation extends Model
 {
     //
 }
--- database/migrations/2018_05_05_124538_create_reservations_table.php
+++ database/migrations/2018_05_05_124538_create_reservations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReservesTable extends Migration
+class CreateReservationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateReservesTable extends Migration
      */
     public function up()
     {
-        Schema::create('reserves', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('page_id')->unsigned();
             $table->foreign('page_id')->references('id')->on('pages');
@@ -33,6 +33,6 @@ class CreateReservesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reserves');
+        Schema::dropIfExists('reservations');
     }
 }
