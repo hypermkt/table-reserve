c03a46835fbdbe220a1f8911239daee609ad28c0
Makoto Chiba <hypermkt@gmail.com>
2018-05-05 13:57:23 +0900

    ツイッター認証できないバグがあった

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,10 @@ Route::resource('pages/{pageId}/courses', 'CourseController')->middleware('auth'
 Route::resource('pages/{pageId}/stocks', 'StockController')->middleware('auth');
 Route::resource('pages', 'PageController')->middleware('auth');
 
-Route::get('/{username}/{pageId}', 'ReservationController@index');
-Route::get('/{username}/{pageId}/courses/1', 'ReservationController@show');
-
 Route::get('auth/twitter', 'Auth\SocialLoginController@redirectToProvider');
 Route::get('auth/twitter/callback', 'Auth\SocialLoginController@handleProviderCallback');
 Route::get('auth/twitter/logout', 'Auth\SocialLoginController@logout');
+
+Route::get('/{username}/{pageId}', 'ReservationController@index');
+Route::get('/{username}/{pageId}/courses/1', 'ReservationController@show');
+
