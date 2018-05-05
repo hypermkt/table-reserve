75fcb9efedd0e0505cf3766802365837b0974ce8
Makoto Chiba <hypermkt@gmail.com>
2018-05-05 13:41:00 +0900

    テストを修正

--- tests/Feature/ReservationTest.php
+++ tests/Feature/ReservationTest.php
@@ -8,15 +8,31 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ReservationTest extends TestCase
 {
+    protected $page;
+    protected $baseUrl;
+
+    public function setUp()
+    {
+        // MEMO: Laravelで独自のsetUpを呼ぶ場合は、parent::setUpの呼び出しが必要
+        // refs: https://readouble.com/laravel/5.6/ja/testing.html
+        parent::setUp();
+        $user = factory(\App\User::class)->create();
+        $this->page = factory(\App\Page::class)->create([
+            'user_id' => $user->id
+        ]);
+        $this->baseUrl = $user->name . '/' . $this->page->id;
+        $this->actingAs($user);
+    }
+
     public function testIndex()
     {
-        $response = $this->get('/reservations/courses');
+        $response = $this->get($this->baseUrl);
         $response->assertstatus(200);
     }
 
     public function testShow()
     {
-        $response = $this->get('/reservations/courses/1');
+        $response = $this->get($this->baseUrl . '/courses/1');
         $response->assertstatus(200);
     }
 }
