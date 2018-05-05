35925bbbdbfa63889c764b4a4fde41a4b4fac5aa
Makoto Chiba <hypermkt@gmail.com>
2018-05-05 17:02:48 +0900

    コース一覧を表示する

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Page;
 
 class ReservationController extends Controller
 {
-    public function index()
+    public function index($username, $pageId)
     {
-        return view('reservations.index');
+        $courses = DB::table('pages')
+            ->join('users', 'pages.user_id', '=', 'users.id')
+            ->join('courses', 'users.id', '=', 'courses.user_id')
+            ->where('pages.id', $pageId)
+            ->where('pages.release_state', 'public')
+            ->where('courses.release_state', 'public')
+            ->select('courses.*')
+            ->get();
+
+        return view('reservations.index', [
+            'courses' => $courses
+        ]);
     }
 
     public function show(Request $id)
