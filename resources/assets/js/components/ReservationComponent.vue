c5c330366a2ee4961395301ed120aae8ce74fd07
Makoto Chiba <hypermkt@gmail.com>
2018-05-06 22:02:15 +0900

    選択した項目の値が取得できるように調整

--- resources/assets/js/components/ReservationComponent.vue
+++ resources/assets/js/components/ReservationComponent.vue
@@ -2,22 +2,22 @@
     <div>
         <div>
             <ul>
-                <li v-for="course in this.courses">
-                    <el-radio v-model="form.radio" :label="course.id">{{ course.title }}</el-radio>
+                <li v-for="item in this.courses">
+                    <el-radio v-model="course" :label="item.id">{{ item.title }}</el-radio>
                 </li>
             </ul>
         </div>
         <div>
             <v-date-picker
                     mode='single'
-                    v-model='selectedDate'
+                    v-model='date'
                     is-inline>
             </v-date-picker>
         </div>
 
-        <el-select v-model="value" placeholder="Select">
+        <el-select v-model="number_of_people" placeholder="Select">
             <el-option
-                    v-for="item in options"
+                    v-for="item in this.number_of_people_options"
                     :key="item.value"
                     :label="item.label"
                     :value="item.value">
@@ -25,11 +25,10 @@
         </el-select>
 
         <div>
-            <el-button>10:00</el-button>
-            <el-button>10:30</el-button>
-            <el-button>11:00</el-button>
-            <el-button>11:30</el-button>
-            <el-button>12:00</el-button>
+            <el-button
+                v-for="item in this.times"
+                :key="item.label"
+                @click="time=item.label">{{ item.label }}</el-button>
         </div>
 
         <div>
@@ -47,8 +46,7 @@
                 </el-form-item>
 
                 <el-form-item>
-                    <el-button type="primary" @click="onSubmit">Create</el-button>
-                    <el-button>Cancel</el-button>
+                    <el-button type="primary" @click="reserve">予約する</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -63,17 +61,25 @@ export default {
     data() {
        return {
            courses: [],
-           options: [
+           number_of_people_options: [
                { label: '1人', value: 1 },
                { label: '2人', value: 2 },
                { label: '3人', value: 3 },
            ],
-           selectedDate: null,
+           times: [
+               { label: '10:00' },
+               { label: '10:30' },
+               { label: '11:00' },
+               { label: '11:30' },
+           ],
+           course: '',
+           number_of_people: '',
+           date: null,
+           time: null,
            form: {
                name: '',
                email: '',
                tel: '',
-               radio: '',
            },
        }
     },
@@ -93,8 +99,14 @@ export default {
                 that.courses = response.data.courses;
             })
         },
-        onSubmit: function() {
-
+        reserve: function() {
+            console.log("course:" + this.course);
+            console.log("date:" + this.date);
+            console.log("time:" + this.time);
+            console.log("number_of_people:" + this.number_of_people);
+            console.log("form.name:" + this.form.name);
+            console.log("form.email:" + this.form.email);
+            console.log("form.tel:" + this.form.tel);
         }
     }
 }
