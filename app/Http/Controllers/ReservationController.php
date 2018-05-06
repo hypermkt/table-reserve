6f963301b8124ecfb9c8e3420e89b3adfd14638e
Makoto Chiba <hypermkt@gmail.com>
2018-05-06 17:49:19 +0900

    不要になったロジックを削除

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,23 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ReservationController extends Controller
 {
     public function index($username)
     {
-        $courses = DB::table('restaurants')
-            ->join('users', 'restaurants.user_id', '=', 'users.id')
-            ->join('courses', 'users.id', '=', 'courses.user_id')
-            ->where('users.name', $username)
-            ->where('restaurants.release_state', 'public')
-            ->where('courses.release_state', 'public')
-            ->select('courses.*')
-            ->get();
-
         return view('reservations.index', [
-            'courses' => $courses,
             'username' => $username,
         ]);
     }
--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -1,17 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>コース一覧</h2>
-    <table>
-        @foreach ($courses as $course)
-        <tr>
-            <td>{{ $course->title }}</td>
-            <td>
-                <a href="/{{ $username }}/{{ $course->id }}">予約する</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-
     <reservation-component></reservation-component>
 @endsection
