22a3a61b72c1fcdbdaf3e01d820fc39d2d093a80
Makoto Chiba <hypermkt@gmail.com>
2018-05-08 23:09:06 +0900

    在庫設定のデザイン調整

--- resources/views/stocks/index.blade.php
+++ resources/views/stocks/index.blade.php
@@ -1,34 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>在庫管理</p>
+    <h2>在庫管理</h2>
+    <h3>{{ $baseDate }}</h3>
 
-    <p><a href="/stocks?month={{ $prevMonth }}">前</a></p>
-    {{ $baseDate }}
-    <p><a href="/stocks?month={{ $nextMonth }}">次</a></p>
-    <form action="/stocks" method="post">
-        {{ @csrf_field() }}
-    <table border="1">
-        <tr>
-            <td>日付</td>
-            @foreach ($tableTypes as $tableType)
-                <td>{{ $tableType->title }}</td>
-            @endforeach
-        </tr>
-        @for ($i = 0; $i < count($stocksTable); $i++ )
-        <tr>
-            <td>{{ $stocksTable[$i]['formatted_date'] }}</td>
-            @foreach ($tableTypes as $tableType)
-            <td>
-                <p><input name="accept_dates[{{ $i }}:{{ $tableType->id }}]" type="checkbox" value="{{ $stocksTable[$i]['accept_date_value'] }}" @isset($stocksTable[$i]['acceptable_table_number']) checked @endisset>予約受付</p>
-                <p><input name="acceptable_table_numbers[{{ $i }}:{{ $tableType->id }}]" type="text" size="5" value="{{ $stocksTable[$i]['acceptable_table_number']  }}">卓</p>
-            </td>
-            @endforeach
-        </tr>
-        @endfor
-    </table>
-        <input type="hidden" name="baseDate" value="{{ $baseDate }}">
-        <input type="submit" value="送信">
-    </form>
-<a href="/">トップページに戻る</a>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="/stocks?month={{ $prevMonth }}">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="/stocks?month={{ $nextMonth }}">Next</a></li>
+        </ul>
+    </nav>
+
+    <div class="form-group">
+        <form action="/stocks" method="post">
+            {{ @csrf_field() }}
+            <table class="table table-bordered bg-white" border="1">
+                <tr>
+                    <td>日付</td>
+                    @foreach ($tableTypes as $tableType)
+                        <td>{{ $tableType->title }}</td>
+                    @endforeach
+                </tr>
+                @for ($i = 0; $i < count($stocksTable); $i++ )
+                    <tr>
+                        <td>{{ $stocksTable[$i]['formatted_date'] }}</td>
+                        @foreach ($tableTypes as $tableType)
+                            <td>
+                                <p><input name="accept_dates[{{ $i }}:{{ $tableType->id }}]" type="checkbox" value="{{ $stocksTable[$i]['accept_date_value'] }}" @isset($stocksTable[$i]['acceptable_table_number']) checked @endisset>予約受付</p>
+                                <p><input name="acceptable_table_numbers[{{ $i }}:{{ $tableType->id }}]" type="text" size="5" value="{{ $stocksTable[$i]['acceptable_table_number']  }}">卓</p>
+                            </td>
+                        @endforeach
+                    </tr>
+                @endfor
+            </table>
+            <input type="hidden" name="baseDate" value="{{ $baseDate }}">
+            <input class="btn btn-primary" type="submit" value="送信">
+        </form>
+    </div>
+    <a class="btn btn-light" href="/">トップページに戻る</a>
 @endsection
