d3f166048bcde0365fae57dae2accfd7e07a639e
Makoto Chiba <hypermkt@gmail.com>
2018-05-10 01:02:17 +0900

    編集画面も

--- resources/views/table_types/edit.blade.php
+++ resources/views/table_types/edit.blade.php
@@ -11,39 +11,39 @@
             @method('put')
             <table class="table table-bordered bg-white" border="1">
                 <tr>
-                    <th>公開状態</th>
+                    <th>公開状態&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td>
                         公開<input type="radio" name="release_state" value="public" @if ($tableType->release_state == 'public') checked @endif>
                         非公開<input type="radio" name="release_state" value="private" @if ($tableType->release_state == 'private') checked @endif>
                     </td>
                 </tr>
                 <tr>
-                    <th>席名称</th>
+                    <th>席名称&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td><input class="form-control" type="text" name="title" value="{{ $tableType->title }}"></td>
                 </tr>
                 <tr>
-                    <th>利用開始時間</th>
+                    <th>利用開始時間&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td><input class="form-control" type="text" name="start_time" value="{{ \Carbon\Carbon::parse($tableType->start_time)->format('H:i') }}"></td>
                 </tr>
                 </tr>
                 <tr>
-                    <th>利用終了時間</th>
+                    <th>利用終了時間&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td><input class="form-control" type="text" name="end_time" value="{{ \Carbon\Carbon::parse($tableType->end_time)->format('H:i') }}"></td>
                 </tr>
                 <tr>
-                    <th>最低定員数</th>
+                    <th>最低定員数&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td><input class="form-control" type="text" name="minimum_capacity" value="{{ $tableType->minimum_capacity }}"></td>
                 </tr>
                 <tr>
-                    <th>最大定員数</th>
+                    <th>最大定員数&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td><input class="form-control" type="text" name="max_capacity" value="{{ $tableType->max_capacity }}"></td>
                 </tr>
                 <tr>
-                    <th>販売可能数</th>
+                    <th>販売可能数&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td><input class="form-control" type="text" name="number_of_sales" value="{{ $tableType->number_of_sales }}"></td>
                 </tr>
                 <tr>
-                    <th>コネクト</th>
+                    <th>コネクト&nbsp;<span class="badge badge-danger">必須</span></th>
                     <td>
                         可<input type="radio" name="connectable" value="1" @if ($tableType->connectable == '1') checked @endif>
                         不可<input type="radio" name="connectable" value="0" @if ($tableType->connectable == '0') checked @endif>
