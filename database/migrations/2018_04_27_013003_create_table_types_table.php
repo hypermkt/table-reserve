89637a3c25d88d56eecd1ef0d8ea12a0c2038f99
Makoto Chiba <hypermkt@gmail.com>
2018-05-11 08:29:58 +0900

    バリデーションメッセージを個別で指定できるようにするため、カラム名を変更

--- database/migrations/2018_04_27_013003_create_table_types_table.php
+++ database/migrations/2018_04_27_013003_create_table_types_table.php
@@ -20,9 +20,9 @@ class CreateTableTypesTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->enum('release_state', ['public', 'private']); // 公開状態
-            $table->string('title'); // 席名称
-            $table->time('start_time'); // 利用開始時間
-            $table->time('end_time'); // 利用終了時間
+            $table->string('table_type_name'); // 席名称
+            $table->time('available_start_time'); // 利用開始時間
+            $table->time('available_end_time'); // 利用終了時間
             $table->integer('minimum_capacity'); // 最低定員数
             $table->integer('max_capacity'); // 最大定員数
             $table->integer('number_of_sales'); // 販売可能数
