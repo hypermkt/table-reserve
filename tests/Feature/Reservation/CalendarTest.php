b0a6b7d2422e3dad7ee668023a95b692b44bf01a
Makoto Chiba <hypermkt@gmail.com>
2018-05-12 20:35:32 +0900

    ステータスを確認するテストケースを追加

---
+++ tests/Feature/Reservation/CalendarTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Reservation;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\DataAccess\Eloquent\User;
+use App\DataAccess\Eloquent\Restaurant;
+use App\DataAccess\Eloquent\TableType;
+
+class CalendarTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $tableType;
+    public function setUp()
+    {
+        parent::setUp();
+        $user = factory(User::class)->create();
+        $restaurant = factory(Restaurant::class)->create([
+            'user_id' => $user->id,
+        ]);
+        $this->tableType = factory(TableType::class)->create([
+            'restaurant_id' => $restaurant->id,
+            'user_id' => $user->id,
+        ]);
+        $this->actingAs($user);
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get('/reservations/calendars');
+        $response->assertStatus(200);
+    }
+}
