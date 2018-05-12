483d7108af5e8e274dab72b33fbbf5904d111417
Makoto Chiba <hypermkt@gmail.com>
2018-05-12 21:58:19 +0900

    予約カレンダーページにFull Calendar 月表示を配置

--- resources/assets/js/components/Reservation/CalendarComponent.vue
+++ resources/assets/js/components/Reservation/CalendarComponent.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
-        CalendarComponent
+        <full-calendar defaultView="month"></full-calendar>
     </div>
 </template>
 
 <script>
-
+export default {
+}
 </script>
+
+<style>
+    @import '~fullcalendar/dist/fullcalendar.css';
+</style>
