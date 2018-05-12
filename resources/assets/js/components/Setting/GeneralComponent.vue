3b7e57c273c7d1e10648e2e9f72a1c8cdbb76f75
Makoto Chiba <hypermkt@gmail.com>
2018-05-13 00:57:52 +0900

    基本設定に公開設定のスイッチを配置

--- resources/assets/js/components/Setting/GeneralComponent.vue
+++ resources/assets/js/components/Setting/GeneralComponent.vue
@@ -1,10 +1,26 @@
 <template>
     <div>
-        general
+        <table>
+            <tr>
+                <td>予約ページ公開設定</td>
+                <td>
+                    <el-switch
+                            v-model="value2"
+                            active-color="#13ce66"
+                            inactive-color="#ff4949">
+                    </el-switch>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-    export default {
+export default {
+    data() {
+        return {
+            value2: false
+        }
     }
+}
 </script>
