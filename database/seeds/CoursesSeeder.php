0b27da64fb3449de69f7be4599596f4764f1f82f
Makoto Chiba <hypermkt@gmail.com>
2018-05-12 10:49:37 +0900

    シーダーのカラム名を修正し忘れていた

--- database/seeds/CoursesSeeder.php
+++ database/seeds/CoursesSeeder.php
@@ -15,7 +15,7 @@ class CoursesSeeder extends Seeder
             'user_id' => App\User::first()->id,
             'release_state' => 'public',
             'kind' => 'course_menu',
-            'title' => '店長オススメ！2時間飲み放題付『国産牛すき焼きと刺身７点盛りプラン』',
+            'course_name' => '店長オススメ！2時間飲み放題付『国産牛すき焼きと刺身７点盛りプラン』',
             'price' => 10000,
             'duration_minutes' => 120,
         ]);
@@ -24,7 +24,7 @@ class CoursesSeeder extends Seeder
             'user_id' => App\User::first()->id,
             'release_state' => 'public',
             'kind' => 'course_menu',
-            'title' => '2時間飲み放題付『牛タンの野菜蒸しコース』4,000円(打上げ　歓送迎会　飲み会　女子会)',
+            'course_name' => '2時間飲み放題付『牛タンの野菜蒸しコース』4,000円(打上げ　歓送迎会　飲み会　女子会)',
             'price' => 8000,
             'duration_minutes' => 120,
         ]);
