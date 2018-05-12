5b261bd9b1e4272a8bc5f6d36b03b82ac3cbb9f4
Makoto Chiba <hypermkt@gmail.com>
2018-05-12 09:13:15 +0900

    カラム名を変更

--- database/migrations/2018_04_27_013953_create_courses_table.php
+++ database/migrations/2018_04_27_013953_create_courses_table.php
@@ -19,7 +19,7 @@ class CreateCoursesTable extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->enum('release_state', ['public', 'private']); // 公開状態
             $table->enum('kind', ['course_menu', 'only_table']); // 区分
-            $table->string('title'); // メニュー名
+            $table->string('course_name'); // メニュー名
             $table->integer('price'); // 料金
             $table->integer('duration_minutes'); // 滞在時間(分)
             $table->timestamps();
