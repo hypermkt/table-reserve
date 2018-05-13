c13e27330a0172b234409de8c15cc9ae57e99f07
Makoto Chiba <hypermkt@gmail.com>
2018-05-13 19:00:00 +0900

    仮の表示例を設定

--- resources/assets/js/components/Reservation/CalendarComponent.vue
+++ resources/assets/js/components/Reservation/CalendarComponent.vue
@@ -1,11 +1,35 @@
 <template>
     <div>
-        <full-calendar defaultView="month"></full-calendar>
+        <full-calendar
+                defaultView="month"
+                editable="false"
+                :events="events"></full-calendar>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            events: [
+                {
+                    id: 1,
+                    title  : '予約: 0組(0名)',
+                    start  : '2018-05-01',
+                    color: '#0000cd',
+                    textColor: '#FFFFFF',
+                },
+                {
+                    id: 2,
+                    title  : '受付中: 23卓',
+                    start  : '2018-05-01',
+                    color: '#ffffff',
+                    textColor: '#808080',
+                },
+            ],
+            color: 'black'
+        }
+    }
 }
 </script>
 
