50bbad0d482f2278f5ada3eae0d018f22012ae9a
Makoto Chiba <hypermkt@gmail.com>
2018-05-18 00:20:44 +0900

    未使用のルーティングを削除

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('v1')->group(function() {
     Route::get('courses', 'Api\V1\CourseController@index');
     Route::post('reservations', 'Api\V1\ReservationController@store');
