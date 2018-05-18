d7ff706db55812344829001766a4e93cb2751cbd
Makoto Chiba <hypermkt@gmail.com>
2018-05-19 00:08:23 +0900

    サービスクラスにビジネスロジックを移譲した

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\DataAccess\Eloquent\TableType;
 use App\DataAccess\Eloquent\Stock;
 use Auth;
+use App\Services\Stock\ListService;
 
 class StockController extends Controller
 {
@@ -15,31 +15,9 @@ class StockController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index(ListService $service)
     {
-        $baseDate = $request->month ?? Carbon::now()->format('Y-m');
-
-        $acceptableTableNumbers = Stock::where('accept_date', '>=', Carbon::parse($baseDate)->format('Y-m-01'))
-            ->where('accept_date', '<', Carbon::parse($baseDate)->addMonth(1)->format('Y-m-01'))
-            ->get()
-            ->pluck('acceptable_table_number', 'accept_date')
-            ->toArray();
-
-        $stocksTable = [];
-        for ($i = 0; $i < Carbon::parse($baseDate)->daysInMonth; $i++) {
-            $date = $baseDate . '-' . sprintf('%02d', ($i + 1));
-            $stocksTable[$i]['formatted_date'] = Carbon::parse($date)->format('Y-m-d(D)');
-            $stocksTable[$i]['accept_date_value'] = Carbon::parse($date)->format('Y-m-d');
-            $stocksTable[$i]['acceptable_table_number'] = $acceptableTableNumbers[$date] ?? null;
-        }
-
-        return view('stocks.index', [
-            'tableTypes' => TableType::all(),
-            'baseDate' => $baseDate,
-            'stocksTable' => $stocksTable,
-            'nextMonth' => Carbon::parse($baseDate)->addMonth(1)->format('Y-m'),
-            'prevMonth' => Carbon::parse($baseDate)->addMonth(-1)->format('Y-m'),
-        ]);
+        return view('stocks.index', $service->execute());
     }
 
     /**
